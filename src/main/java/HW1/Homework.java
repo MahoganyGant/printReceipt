d3a024693d336534b6459f46ee891db7e74d70dc
MahoganyGant <mahoganygant02@gmail.com>
2024-10-15 16:31:02 -0400

    updated code to use string.format, not finished yet

--- src/main/java/HW1/Homework.java
+++ src/main/java/HW1/Homework.java
@@ -10,24 +10,25 @@ public class Homework {
     public static void printReceipt(){
         //Instantiating what we know
         Date date = new Date();
-        String closingStatement = "Thank you. Come again!!!";
+        String closingStatement = "\t\tThank you. Come again!!!";
         String purchaseDate = String.format("Purchased on: %tA, %<tB %<td, 2024" , date);
 
         //scanning for all the information, and storing the values we scanned for
         Scanner scan = new Scanner(System.in);
         System.out.println("What is the name of your product?: ");
-        String productName = scan.nextLine();
+        String Name = scan.nextLine();
         System.out.println("How many are you buying today?: ");
         int qty = scan.nextInt();
         System.out.println("What is the price of the product?: ");
         double price = scan.nextDouble();
         double total = price*qty;
-        String fTotal = String.format("%.2f",total);
 
-        System.out.println("-------------------------------------------------------------\n" +
-                "Name    Qty    Price    Total\n" +
-                "-----   ----   -----   ------\n" + productName + "     " + qty + "     $" + price + "     $" + fTotal + "\n \n" + closingStatement + "\n \n" + purchaseDate
-        + "\n-------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------\n");
+        System.out.printf("%-10s %5s %10s %10s%n", "Name","Qty","Price","Total");
+        System.out.printf("%-10s %5s %10s %10s%n", "-----","-----","-----","-----\n");
+        System.out.printf("%-10s %5d %10.2f %10.2f%n",Name,qty,price,total);
+        System.out.println("\n" + closingStatement + "\n \n" + purchaseDate);
+        System.out.println("\n-------------------------------------------------------------");
     }
 
     public static void main(String[] args) {
