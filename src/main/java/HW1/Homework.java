bfca3c25f9889af109e26e7f7c24ac34737cdccd
MahoganyGant <mahoganygant02@gmail.com>
2024-10-16 22:06:39 -0400

    Added small demo to understand variable scope

--- src/main/java/HW1/Homework.java
+++ src/main/java/HW1/Homework.java
@@ -4,6 +4,28 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Homework {
+ /*
+    Additonal Homework questions to tackle after printReceipt HW:
+    1. How do we get rid of an object?
+    2. create a small demo to understand variable scope in java
+    3. what's the constructor called that has parameters
+     */
+
+
+    //Small demo variable scope
+     /*
+        step one-uncomment out line 59, view the error
+
+        step two-uncomment out line 28, notice that didn't fix the error, although we declared the variable outside the method that didn't allow us
+        to access the variable 'Name' we intended to print out. This is because the variable 'Name' we wanted to print is a local variable meaning it only
+        exist within the block of code it's declared in. As the variable Name declared at the top of the class is not the same variable 'Name' inside the method
+
+        step three-Add the non access modifier 'static' at the beginning of the statement on line 28, if you test this at this point you will get a null return
+        because we are still not accessing the variable Name within the printReceipt method. In order to access the same variable we need to take out the data
+        type we placed in front of the variable name on line 41 to tell the program they are the same variable
+         */
+
+  // String Name;
 
 
     //create a method named printReceipt
@@ -33,5 +55,8 @@ public class Homework {
 
     public static void main(String[] args) {
        printReceipt();
+
+        // System.out.println(Name);
+
     }
 }
